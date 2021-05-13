d6f8888edfb0f5e878dd78e5e2b7899842ecc40d
hyochan-brown <brown.kim@groundx.xyz>
2021-05-13 13:19:33 +0900

    refactor: Rename node/utils.go to node/api.go

--- node/api_test.go
+++ node/api_test.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 //
-// This file is derived from node/utils_test.go (2018/06/04).
+// This file is derived from node/api_test.go (2018/06/04).
 // Modified and improved for the klaytn development.
 
 // Contains a batch of utility type declarations used by the tests. As the node
